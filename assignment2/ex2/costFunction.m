bd0a1e613082c278653acf540fa3d74b85c99a4b
Neil Lakin <neil.lakin@gmail.com>
2015-11-18 18:08:23 -0500

    implement gradient descent

--- assignment2/ex2/costFunction.m
+++ assignment2/ex2/costFunction.m
@@ -7,18 +7,12 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
+sig = sigmoid(X*theta);
+J = (1/m)*((-y'*log(sig))-(-y+1)'*log(1-sig))
+grad = (1/m)*X'*(sig-y)
 
 
 
