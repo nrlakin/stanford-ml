2bb5e316d8adb226ae2c64a929b87391006789fc
Neil Lakin <neil.lakin@gmail.com>
2015-11-30 13:49:34 -0600

    one-vs-all prediction working

--- assignment3/ex3/predictOneVsAll.m
+++ assignment3/ex3/predictOneVsAll.m
@@ -14,32 +14,8 @@ num_labels = size(all_theta, 1);
 % You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
-% Add ones to the X data matrix
-X = [ones(m, 1) X];
-p_s=X*all_theta';
-p=max(p_s, [], 2);
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters (one-vs-all).
-%               You should set p to a vector of predictions (from 1 to
-%               num_labels).
-%
-% Hint: This code can be done all vectorized using the max function.
-%       In particular, the max function can also return the index of the
-%       max element, for more information see 'help max'. If your examples
-%       are in rows, then, you can use max(A, [], 2) to obtain the max
-%       for each row.
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
+Xs = [ones(m, 1) X];
+p_s = sigmoid(Xs*all_theta');
+[m p] = max(p_s, [], 2);
 
 end
